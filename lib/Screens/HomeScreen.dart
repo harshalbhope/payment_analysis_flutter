30c4a7cf20a219e46068b479ad0685ba149d4b2d
harshalbhope <harshalbhope67@gmail.com>
2020-07-13 01:11:05 +0530

    Cards_added

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
+// import 'package:payment_analysis_flutter/models/CreditCards.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
+
+class CreditCards {
+  String balance;
+  String cardNo;
+  String cardName;
+  Color color;
+
+  CreditCards({this.balance, this.cardNo, this.cardName, this.color});
+}
+
+List<CreditCards> creditcards = [
+  CreditCards(
+      balance: '2000', cardNo: '1123', cardName: 'HDFC', color: Colors.grey),
+  CreditCards(
+      balance: '3000', cardNo: '543', cardName: 'HDFC', color: Colors.yellow),
+  CreditCards(
+      balance: '4000', cardNo: '333', cardName: 'HDFC', color: Colors.green)
+];
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
@@ -16,7 +35,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Stack(
               children: <Widget>[
                 Container(
-                  height: MediaQuery.of(context).size.width * 0.80,
+                  height: MediaQuery.of(context).size.height * 0.50,
                   decoration: BoxDecoration(
                     color: Theme.of(context).primaryColorDark,
                     borderRadius: BorderRadius.only(
@@ -85,11 +104,115 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     ],
                   ),
-                )
+                ),
+                BottomContainer(),
               ],
-            )
+            ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class BottomContainer extends StatelessWidget {
+  const BottomContainer({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.40),
+      // // height: MediaQuery.of(context).size.height - 20,
+      height: 355.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      padding: EdgeInsets.only(top: 30.0, right: 20.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: Padding(
+              padding: EdgeInsets.only(left: 20.0),
+              child: Text(
+                'Choose Card',
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Container(
+            height: 200,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: creditcards.length,
+              itemBuilder: (context, index) {
+                CreditCards creditcard = creditcards[index];
+                print(creditcard.balance);
+                return Container(
+                  width: 300,
+                  padding: EdgeInsets.only(left: 20.0),
+                  child: Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    color: creditcard.color,
+                    child: Padding(
+                      padding:
+                          EdgeInsets.only(top: 40, left: 15.0, bottom: 15.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Balance',
+                            style: TextStyle(
+                              color: Colors.white54,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 5.0,
+                          ),
+                          Text(
+                            creditcard.balance,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              child: Align(
+                                alignment: FractionalOffset.bottomLeft,
+                                child: Text(
+                                  creditcard.cardName,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/models/CreditCards.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CreditCards {
+  String balance;
+  String cardNo;
+  String cardName;
+  Color color;
+
+  CreditCards({this.balance, this.cardNo, this.cardName, this.color});
+}
+
+List<CreditCards> creditcards = [
+  CreditCards(
+      balance: '2000', cardNo: '1123', cardName: 'HDFC', color: Colors.black),
+  CreditCards(
+      balance: '3000', cardNo: '543', cardName: 'HDFC', color: Colors.yellow),
+  CreditCards(
+      balance: '4000', cardNo: '333', cardName: 'HDFC', color: Colors.green)
+];
